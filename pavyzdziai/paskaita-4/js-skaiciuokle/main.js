43bbb7c9a54d4b8dbbed90161d71da992495fc5c
Laurynas <laurynas.ant@gmail.com>
2017-02-17 15:42:02 +0200

    ciklas

--- pavyzdziai/paskaita-4/js-skaiciuokle/main.js
+++ pavyzdziai/paskaita-4/js-skaiciuokle/main.js
@@ -8,8 +8,15 @@ function firstName () {
   return "Laurynas";
 }
 
-console.log(firstName());
-console.log(greeting("Laurynas"));
-console.log(greeting(firstName()));
+var names = ["Jonas", "Laurynas", "Petras"];
+names.push("Ona");
+
+for (var i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+
+// console.log(firstName());
+// console.log(greeting("Laurynas"));
+// console.log(greeting(firstName()));
 
 // console.log(typeof(greeting), greeting());
