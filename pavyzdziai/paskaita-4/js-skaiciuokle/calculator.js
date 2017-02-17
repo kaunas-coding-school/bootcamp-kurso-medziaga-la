29c2a2781581c50587461f7275e880f911b92e3b
Laurynas <laurynas.ant@gmail.com>
2017-02-17 17:37:27 +0200

    kalkuliatorius, baigtas

--- pavyzdziai/paskaita-4/js-skaiciuokle/calculator.js
+++ pavyzdziai/paskaita-4/js-skaiciuokle/calculator.js
@@ -1,11 +1,9 @@
-console.log("Labas");
-
 // 1. Pasirenkam HTML el
 var screenInput = document.querySelector("#js-screen");
-console.log(screenInput);
-
 var numberInput = document.querySelectorAll(".js-number");
-console.log(numberInput);
+var operatorInput = document.querySelectorAll(".js-operator");
+var resultInput = document.querySelector("#js-result");
+var firstNumber, operatorValue;
 
 // 2. Stebim arba vykdom event'a
 // screenInput.addEventListener();
@@ -14,12 +12,43 @@ for (var i = 0; i < numberInput.length; i++) {
   numberInput[i].addEventListener("click", numberClick);
 }
 
-// 3. Sukuriam, vykdom funkcija
-function numberClick(event) {
-  screenInput.value = event.target.value;
-  console.log(event);
+for (var i = 0; i < operatorInput.length; i++) {
+  operatorInput[i].addEventListener("click", operatorClick);
 }
 
-console.log(numberClick);
+resultInput.addEventListener("click", resultClick);
 
-// Ekrano reiksmes atnaujinimas
+// 3. Sukuriam, vykdom funkcija
+function numberClick(event) {
+  if (screenInput.value === "0") {
+    screenInput.value = event.target.value;
+  } else {
+    screenInput.value += event.target.value;
+  }
+}
+
+function operatorClick(event) {
+  firstNumber = screenInput.value;
+  operatorValue = event.target.value;
+  screenInput.value = "0";
+}
+
+function resultClick() {
+  var result;
+  var secondNumber = screenInput.value;
+  switch (operatorValue) {
+    case "+":
+      result = parseInt(firstNumber) + parseInt(secondNumber);
+      break;
+    case "/":
+      result = firstNumber / secondNumber;
+      break;
+    case "-":
+      result = firstNumber - secondNumber;
+      break;
+    case "x":
+      result = firstNumber * secondNumber;
+      break;
+  }
+  screenInput.value = result;
+}
