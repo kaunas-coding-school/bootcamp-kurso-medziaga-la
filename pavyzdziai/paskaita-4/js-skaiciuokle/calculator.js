62d0a2e7e6fb5ecad4f2d87ed7dddebac3664270
Laurynas <laurynas.ant@gmail.com>
2017-02-17 17:09:25 +0200

    pavyzdys su screen'u

--- pavyzdziai/paskaita-4/js-skaiciuokle/calculator.js
+++ pavyzdziai/paskaita-4/js-skaiciuokle/calculator.js
@@ -1,1 +1,25 @@
 console.log("Labas");
+
+// 1. Pasirenkam HTML el
+var screenInput = document.querySelector("#js-screen");
+console.log(screenInput);
+
+var numberInput = document.querySelectorAll(".js-number");
+console.log(numberInput);
+
+// 2. Stebim arba vykdom event'a
+// screenInput.addEventListener();
+
+for (var i = 0; i < numberInput.length; i++) {
+  numberInput[i].addEventListener("click", numberClick);
+}
+
+// 3. Sukuriam, vykdom funkcija
+function numberClick(event) {
+  screenInput.value = event.target.value;
+  console.log(event);
+}
+
+console.log(numberClick);
+
+// Ekrano reiksmes atnaujinimas
