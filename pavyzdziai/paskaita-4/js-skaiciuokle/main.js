0391c331a2b982e9e969788fdaac9d581bfa8fc1
Laurynas <laurynas.ant@gmail.com>
2017-02-17 16:16:34 +0200

    pridetas uzdavinys ir pradiniai failai

--- pavyzdziai/paskaita-4/js-skaiciuokle/main.js
+++ pavyzdziai/paskaita-4/js-skaiciuokle/main.js
@@ -1,19 +1,34 @@
-console.log(window);
-
-var greeting = function(firstName){
-  return "Hello " + firstName;
-};
-
-function firstName () {
-  return "Laurynas";
-}
+var x = 2, y, z;
 
 var names = ["Jonas", "Laurynas", "Petras"];
 names.push("Ona");
 
-for (var i = 0; i < names.length; i++) {
-  console.log(names[i]);
+console.log(window);
+
+var greeting = function(firstName){
+  console.log(firstName);
+  return "Hello " + firstName;
+};
+
+function firstName2 () {
+  return "Laurynas";
 }
+
+for (var i = 0; i < names.length; i++) {
+  // console.log(names[i]);
+}
+
+function A () {
+  var x = 5;
+  console.log(x);
+  B();
+}
+
+function B () {
+  console.log(x);
+}
+
+A();
 
 // console.log(firstName());
 // console.log(greeting("Laurynas"));
