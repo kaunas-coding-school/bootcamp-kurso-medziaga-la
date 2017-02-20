5ef923487aa2a7922258340fa5806eabf9b97896
Laurynas <laurynas.ant@gmail.com>
2017-02-20 17:17:56 +0200

    uzbaigta validacija

--- pavyzdziai/paskaita-5/contact-form/scripts/main.js
+++ pavyzdziai/paskaita-5/contact-form/scripts/main.js
@@ -40,8 +40,34 @@
   // 2 Add event listener
   contactForm.addEventListener("submit", validateFields);
 
+  // Pridedam pagrindine funkcija, sujungiam su form submit veiksmu
   function validateFields(event) {
-    event.preventDefault();
-    console.log(event.target.firstname.value);
+    // Is sio veiksmo pasiemam reiksmes is paciu laukeliu
+    var fname = event.target.firstname.value;
+    var lname = event.target.lastname.value;
+    var email = event.target.email.value;
+
+    // Pasitikrinam ar bent vienas is siu kintamuju yra tuscias
+    if (fname === "" || lname  === "" || email  === "") {
+      // Stabdom formos siuntima
+      event.preventDefault();
+      // Sukuriam nauja div elementa
+      var errorMessage = document.createElement("div");
+      // Kintamasis patikrinimui ar jau yra message
+      var oldError = document.querySelector(".alert");
+      // Uzpildom nauja elementa turiniu
+      errorMessage.innerHTML = '<p class="alert alert-danger">Please, fill in form fields</p>';
+      // Ikeliam i HTML struktura
+      if (oldError) {
+        contactForm.replaceChild(errorMessage, oldError.parentNode);
+      } else {
+        contactForm.appendChild(errorMessage);
+      }
+
+
+      console.log(errorMessage);
+      console.log(fname, lname, email);
+      return;
+    }
   }
 })();
