1f5b5ea468ffe22bd85a40261f92dad208871102
Laurynas <laurynas.ant@gmail.com>
2017-02-24 14:51:44 +0200

    wiredep itraukimas

--- pavyzdziai/paskaita-8/gulp-sass/gulpfile.js
+++ pavyzdziai/paskaita-8/gulp-sass/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var wiredep = require('wiredep').stream;
 
 // Paleisti serveri
 gulp.task('browser-sync', function(){
@@ -11,8 +12,26 @@ gulp.task('browser-sync', function(){
   });
 });
 
+// Kompiliuoti CSS
 gulp.task('styles', function() {
   return gulp.src('./styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/styles'));
+    .pipe(gulp.dest('./dist/styles'))
+    .pipe(browserSync.stream());
 });
+
+gulp.task('wiredep', function() {
+  gulp.src('./index.html')
+    .pipe(wiredep())
+    .pipe(gulp.dest('./'));
+});
+
+// Atnaujinti browser, kai pakeiciami SCSS failai
+// Pradedam developint
+gulp.task('watch', ['wiredep', 'browser-sync', 'styles'], function(){
+  gulp.watch('./styles/**/*.scss', ['styles']);
+});
+
+// Default komanda
+// Perkompiliuoti
+gulp.task('default', ['wiredep', 'styles']);
