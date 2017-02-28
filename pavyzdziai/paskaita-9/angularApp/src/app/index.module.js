5d486ab3a49e2d822bf3bec2c65acc2d04445fe8
Laurynas <laurynas.ant@gmail.com>
2017-02-28 12:11:50 +0200

    pradinis projektas html ir angular

---
+++ pavyzdziai/paskaita-9/angularApp/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'mgcrea.ngStrap', 'toastr']);
+
+})();
