cb4c0700dc9c4269c23e207ac2d8b25c3b1258a6
Laurynas <laurynas.ant@gmail.com>
2017-03-02 19:31:27 +0200

    sprendimas php su db

--- pavyzdziai/paskaita-11/uzklausos-php/index.php
+++ pavyzdziai/paskaita-11/uzklausos-php/index.php
@@ -12,10 +12,23 @@ $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $pdo = new PDO($dsn, $user, $pass);
 
 // Set up query
-$query = $pdo->query('SELECT * FROM users');
 
 // Execute query and save results to $users
 // $users = $query->fetch();
+
+if (array_key_exists('email', $_POST)) {
+  $email = $_POST['email'];
+  $firstname = $_POST['firstname'];
+  $lastname = $_POST['lastname'];
+
+  if ($email != '') {
+    $query = $pdo->prepare('INSERT INTO users VALUES (NULL, :firstname, :lastname, :email)')->execute([
+      ':firstname' => $firstname,
+      ':lastname' => $lastname,
+      ':email' => $email
+    ]);
+  }
+}
 
 ?>
 <!DOCTYPE html>
@@ -26,10 +39,36 @@ $query = $pdo->query('SELECT * FROM users');
   </head>
   <body>
     <h1>Hello world</h1>
-    <?php
-      while ($user = $query->fetch()) {
-        echo $user['id'] . $user['firstname'] . "\n";
-      }
-    ?>
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Firstname</th>
+          <th>Lastname</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php $query = $pdo->query('SELECT * FROM users'); ?>
+        <?php while ($user = $query->fetch()): ?>
+        <tr>
+          <td><?php echo $user['id'] ?></td>
+          <td><?php echo $user['firstname'] ?></td>
+          <td><?php echo $user['lastname'] ?></td>
+          <td><?php echo $user['email'] ?></td>
+        </tr>
+        <?php endwhile; ?>
+      </tbody>
+    </table>
+    <br>
+    <form method="post">
+      <label for="firstname">Your name:</label>
+      <input id="firstname" type="text" name="firstname">
+      <label for="lastname">Your surname:</label>
+      <input id="lastname" type="text" name="lastname">
+      <label for="email">Your email:</label>
+      <input id="email" type="text" name="email">
+      <input type="submit" value="Register">
+    </form>
   </body>
 </html>
