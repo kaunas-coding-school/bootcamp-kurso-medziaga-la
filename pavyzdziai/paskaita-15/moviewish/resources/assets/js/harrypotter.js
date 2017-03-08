92ff408d7da82554688f4875a4969f14b58ac8df
Laurynas <laurynas.ant@gmail.com>
2017-03-08 17:07:06 +0200

    harrypotter test

--- pavyzdziai/paskaita-15/moviewish/resources/assets/js/harrypotter.js
+++ pavyzdziai/paskaita-15/moviewish/resources/assets/js/harrypotter.js
@@ -1,7 +1,89 @@
 var HarryPotter = {};
+// HarryPotter knygu kaina
+HarryPotter.price = 8;
 
-HarryPotter.countBooks = function() {
-    return 0;
+// Galimybe pakeisti knygos kaina
+HarryPotter.setPrice = function(newPrice) {
+    this.price = newPrice;
 };
+
+// Metodas skaiciuoja kiek skirtingu knygu perkam
+// input Array
+// output Number
+HarryPotter.countBooks = function(books) {
+    if (!books){
+        return 0;
+    }
+    return books.length;
+};
+
+// Metodas skaiciuoja kokia nuolaida duoti pagal tai kiek knygu perkam
+// input Number
+// output Number
+HarryPotter.calcDiscount = function(differentBooks) {
+    var discount = 0;
+    switch (differentBooks) {
+        case 2:
+            discount = 5;
+            break;
+        case 3:
+            discount = 10;
+            break;
+        case 4:
+            discount = 20;
+            break;
+        case 5:
+            discount = 25;
+            break;
+    }
+    return discount;
+};
+
+HarryPotter.applyDiscount = function(books) {
+    var count = this.countBooks(books);
+    return this.calcDiscount(count);
+};
+
+HarryPotter.booksTotal = function(books) {
+    var sum = 0;
+    for (var i = 0; i < books.length; i++) {
+        sum += books[i];
+    }
+    return sum;
+};
+
+HarryPotter.cartTotal = function(books) {
+    if (typeof(books) === 'object') {
+        return this.booksTotal(books) * this.price;
+    }
+    return books * this.price;
+};
+
+HarryPotter.cartDiscount = function(books) {
+    var discount = this.applyDiscount(books); // 10%
+    var cartTotal = this.cartTotal(this.countBooks(books)); // books == [2, 1, 1]
+    return cartTotal * discount / 100;  // 10/100*72
+};
+
+HarryPotter.totalPrice = function(books) {
+    var cartTotal = this.cartTotal(books);
+    var booksTotal = this.countBooks(books);
+    var discountSum = 0;
+    var max = Math.max.apply(null, books);
+
+    for (var i = 0; i < max; i++) {
+        discountSum += this.cartDiscount(books);
+        for (var j = 0; j < booksTotal; j++) {
+            books[j] -= 1;
+            if (books[j] === 0) {
+                books.splice(j, 1);
+                j--;
+                booksTotal--;
+            }
+        }
+    }
+    return cartTotal - discountSum;
+};
+
 
 module.exports = HarryPotter;
--- pavyzdziai/paskaita-15/moviewish/tests/HarryPotterTest.js
+++ pavyzdziai/paskaita-15/moviewish/tests/HarryPotterTest.js
@@ -7,5 +7,91 @@ describe('HarryPotter', function() {
         it('should return 0 if no parameters given', function() {
             assert.equal(0, hp.countBooks());
         });
+
+        it('should return 1 if 1 parameter given', function() {
+            assert.equal(0, hp.countBooks([]));
+        });
+
+        it('should return 2 if 2 array values are given', function() {
+            assert.equal(2, hp.countBooks([2, 3]));
+        });
+
+        it('should return 5 if 5 array values are given', function() {
+            assert.equal(5, hp.countBooks([2, 2, 2, 1, 1]));
+        });
+
     });
+
+    describe('calcDiscount()', function(){
+        it('should give no discount if one book is sold', function() {
+            assert.equal(0, hp.calcDiscount(1));
+        });
+
+        it('should give 5% discount if two different books are sold', function() {
+            assert.equal(5, hp.calcDiscount(2));
+        });
+
+        it('should give 10% discount if three different books are sold', function() {
+            assert.equal(10, hp.calcDiscount(3));
+        });
+
+        it('should give 20% discount if four different books are sold', function() {
+            assert.equal(20, hp.calcDiscount(4));
+        });
+
+        it('should give 25% discount if five different books are sold', function() {
+            assert.equal(25, hp.calcDiscount(5));
+        });
+    });
+
+    describe('applyDiscount()', function(){
+        it('should give 25% discount if we buy 5 different books', function() {
+            assert.equal(25, hp.applyDiscount([2, 2, 2, 1, 1]));
+        });
+    });
+
+    describe('booksTotal()', function(){
+        it('should give 9 if we have an array of 9 books', function() {
+            assert.equal(9, hp.booksTotal([3, 2, 2, 1, 1]));
+        });
+    });
+
+    describe('cartTotal()', function(){
+        it('should give $72 total price of 9 if one book costs $8', function() {
+            assert.equal(72, hp.cartTotal(9));
+        });
+
+        it('should give $72 total price of 9 if one book costs $8', function() {
+            assert.equal(72, hp.cartTotal([3, 2, 2, 1, 1]));
+        });
+    });
+
+    describe('cartDiscount()', function(){
+        it('should give $2.4 discount if we buy 3 different books ', function() {
+            assert.equal(2.4, hp.cartDiscount([1, 1, 1]));
+        });
+
+        it('should give $2.4 discount if we buy 3 different books but 4 in total', function() {
+            assert.equal(2.4, hp.cartDiscount([2, 1, 1]));
+        });
+
+        it('should give $6.4 discount if we buy 4 different books but 10 in total', function() {
+            assert.equal(6.4, hp.cartDiscount([4, 3, 2, 1]));
+        });
+    });
+
+    describe('totalPrice()', function(){
+        it('should give $30 total price if we buy 5 different books but 8 in total', function() {
+            assert.equal(30, hp.totalPrice([1, 1, 1, 1, 1]));
+        });
+
+        it('should give $60 total price if we buy 5 different books but 10 in total', function() {
+            assert.equal(60, hp.totalPrice([2, 2, 2, 2, 2]));
+        });
+
+        it('should give $51.6 total price if we buy 5 different books but 8 in total', function() {
+            assert.equal(51.6, hp.totalPrice([2, 2, 2, 1, 1]));
+        });
+    });
+
 });
