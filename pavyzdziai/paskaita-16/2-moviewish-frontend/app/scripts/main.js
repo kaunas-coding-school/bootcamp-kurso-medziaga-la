95e5d5409b7e918c5b2f9323a1716bc6e765fe38
Laurynas <laurynas.ant@gmail.com>
2017-03-09 13:00:27 +0200

    16 paskaita

---
+++ pavyzdziai/paskaita-16/2-moviewish-frontend/app/scripts/main.js
@@ -0,0 +1,7 @@
+(function($){
+    $(document).ready(function () {
+        $('[data-toggle="offcanvas"]').click(function () {
+            $('.row-offcanvas').toggleClass('active');
+        });
+    });
+})(jQuery);
